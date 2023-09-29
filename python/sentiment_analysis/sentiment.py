77cac1ebf693bf2bb32d8ebab3c035c77eb03c18
YuChenHeMTL <yuchen7568@gmail.com>
2023-09-29 10:43:29 -0400

    changed to abspath model

--- python/sentiment_analysis/sentiment.py
+++ python/sentiment_analysis/sentiment.py
@@ -1,8 +1,9 @@
 import fasttext
+import os
 
 # Load the model
 
-model = fasttext.load_model("./sentiment_analysis/model.bin")
+model = fasttext.load_model(os.path.abspath("./sentiment_analysis/model.bin"))
 
 def predict(text):
     prediction = model.predict(text)
